bf96fd96d8fb77d28237e671830cd107f2820d87
Juicestus <jus@gtsbr.org>
2021-10-10 17:50:03 -0500

    Started work on evaluator and object

---
+++ src/eval.cpp
@@ -0,0 +1,13 @@
+#ifndef EVAL_H_
+#define EVAL_H_
+
+#include <cstdarg>
+#include <functional>
+#include <unordered_map>
+
+#include "ast.h"
+#include "object.h"
+#include "env.h"
+#include "gc.h"
+
+#endif 
