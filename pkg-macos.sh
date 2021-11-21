4d232756f4d7327d858428bc44a56c179fb724fe
Juicestus <jus@gtsbr.org>
2021-11-20 22:36:07 -0600

    changed then reverted equality

--- pkg-macos.sh
+++ pkg-macos.sh
@@ -2,9 +2,9 @@
 
 cp -r bin/xt package/mac/application/
 
-bash package/mac/build-macos-x64.sh xt 1.0.0
+bash package/mac/build-macos-x64.sh xt $1
 
-mv package/mac/target/pkg/xt-macos-installer-x64-1.0.0.pkg dist/mac-x86_64-installer
+mv "package/mac/target/pkg/xt-macos-installer-x64-$1.pkg" dist/mac-x86_64-installer
 
 rm -rf package/mac/application/xt
 rm -rf package/mac/target
